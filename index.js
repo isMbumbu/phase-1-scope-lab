4bbc7d73dc927d0a881cc30cc98d3b5ce82a728b
isMbumbu <andrewambuka9@gmail.com>
2024-10-05 14:49:36 +0300

    practice scoping

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+var customerName='bob';
+function upperCaseCustomerName() {
+    // Modify the global variable customerName
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer='not bob';
+}
+function overwriteBestCustomer(){
+    bestCustomer='maybe bob';
+}
+const leastFavoriteCustomer='dere';
+function changeLeastFavoriteCustomer(){
+    return leastFavoriteCustomer='key';
+}
